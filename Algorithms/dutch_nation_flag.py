81ff897511fb8ac2680e2659358d17f7d9537cfc
Niramay Thaker <70151582+NiramayThaker@users.noreply.github.com>
2023-06-16 02:56:39 +0530

    Dutch nation flag algorithm

--- Algorithms/dutch_nation_flag.py
+++ Algorithms/dutch_nation_flag.py
@@ -1,16 +1,17 @@
-def dnf(arr, n):
+def dnf(nums, n):
     s_ptr = 0
-    m_ptr = 1
+    m_ptr = 0
     e_ptr = n - 1
-    
-    while m_ptr < e_ptr:
-        if arr[m_ptr] == 1:
-            arr[s_ptr], arr[m_ptr] = arr[m_ptr], arr[s_ptr]
+
+    while m_ptr <= e_ptr:
+        if nums[m_ptr] == 0:
+            nums[s_ptr], nums[m_ptr] = nums[m_ptr], nums[s_ptr]
             s_ptr += 1
-        elif arr[m_ptr] == 2:
-            arr[e_ptr], arr[m_ptr] = arr[m_ptr], arr[e_ptr]
+            m_ptr += 1
+        elif nums[m_ptr] == 2:
+            nums[e_ptr], nums[m_ptr] = nums[m_ptr], nums[e_ptr]
             e_ptr -= 1
         else:
             m_ptr += 1
-            
-  return arr
+
+    return nums
